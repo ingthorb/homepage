92a7487595741f1f60c8ef60a7ad7d57365beac3
ingthorb <ingthorb@gmail.com>
2023-04-17 19:31:10 +0200

    Set url to https for weather

--- src/utils/Axios/axiosConfig.js
+++ src/utils/Axios/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosConfig = axios.create({
-	baseURL: `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API}&q=Bern&aqi=no`,
+	baseURL: `https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API}&q=Bern&aqi=no`,
 	xsrfCookieName: "csrftoken",
 	xsrfHeaderName: "X-CSRFTOKEN"
 });
